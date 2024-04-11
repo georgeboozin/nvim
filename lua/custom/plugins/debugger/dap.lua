36a1abbea225e9ae3666e14bcee63617ed38d8b1
George Boozin <boozingeorge@gmail.com>
2024-04-11 23:44:14 +0300

    enhance dap colors

--- lua/custom/plugins/debugger/dap.lua
+++ lua/custom/plugins/debugger/dap.lua
@@ -37,6 +37,59 @@ return {
             require("dap").step_out()
         end, { desc = "Step out" })
 
-        -- map("n", "<tab>", "<cmd>BufferNext<CR>", { desc = "Buffer Goto next" })
+        vim.api.nvim_set_hl(0, "DapBreakpointIcon", {
+            ctermbg = 0,
+            fg = "#ed8796",
+        })
+        vim.api.nvim_set_hl(0, "DapBreakpointLine", {
+            ctermbg = 0,
+            -- bg = "#4d4756", -- rosewater
+            -- bg = "#383748", -- lighter rosewater
+            -- bg = "#4c3044", -- red#1
+            bg = "#4d2d41", -- red#2
+        })
+
+        vim.api.nvim_set_hl(0, "DapLogPointIcon", { ctermbg = 0, fg = "#a6da95" })
+        vim.api.nvim_set_hl(0, "DapLogPointLine", {
+            ctermbg = 0,
+            bg = "#4b5d55", -- dgreen
+            -- bg = "#3e4b4c " -- ligher green
+        })
+
+        vim.api.nvim_set_hl(0, "DapStoppedIcon", {
+            ctermbg = 0,
+            -- fg = "#98c379",
+            fg = "#eed49f",
+        })
+        vim.api.nvim_set_hl(0, "DapStoppedLine", { ctermbg = 0, bg = "#48464B" })
+
+        vim.fn.sign_define("DapBreakpoint", {
+            text = "",
+            texthl = "DapBreakpointIcon",
+            linehl = "DapBreakpointLine",
+            -- numhl = "DapBreakpoint",
+        })
+        vim.fn.sign_define("DapBreakpointCondition", {
+            text = "ﳁ",
+            texthl = "DapBreakpointIcon",
+            linehl = "DapBreakpointLine",
+            -- numhl = "DapBreakpoint"
+        })
+        vim.fn.sign_define("DapBreakpointRejected", {
+            text = "",
+            texthl = "DapBreakpointIcon",
+            linehl = "DapBreakpointLine",
+            --numhl = "DapBreakpoint"
+        })
+        vim.fn.sign_define(
+            "DapLogPoint",
+            { text = "", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
+        )
+        vim.fn.sign_define("DapStopped", {
+            text = "",
+            texthl = "DapStoppedIcon",
+            linehl = "DapStoppedLine",
+            -- numhl = "DapStopped"
+        })
     end,
 }
