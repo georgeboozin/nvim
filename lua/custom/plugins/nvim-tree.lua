23443a0b39fad8dcb7410a7f567a914c06779cbc
George Boozin <boozingeorge@gmail.com>
2024-04-19 16:29:34 +0300

    add hotkey for nvim-tree

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -6,7 +6,24 @@ return {
         "nvim-tree/nvim-web-devicons",
     },
     config = function()
-        require("nvim-tree").setup({})
-        vim.keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "Nvimtree Toggle window" })
+        local function my_on_attach(bufnr)
+            local api = require("nvim-tree.api")
+
+            local function opts(desc)
+                return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+            end
+
+            -- default mappings
+            api.config.mappings.default_on_attach(bufnr)
+
+            -- custom mappings
+            vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
+            vim.keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "Nvimtree Toggle window" })
+        end
+
+        -- pass to setup along with your other options
+        require("nvim-tree").setup({
+            on_attach = my_on_attach,
+        })
     end,
 }
