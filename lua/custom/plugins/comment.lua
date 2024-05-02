6aa657d0487bcb99e76c516cf27c95b884ba2244
George Boozin <boozingeorge@gmail.com>
2024-05-02 15:10:37 +0300

    add jsx/tsx comment

--- lua/custom/plugins/comment.lua
+++ lua/custom/plugins/comment.lua
@@ -1,7 +1,13 @@
 return {
     "numToStr/Comment.nvim",
     opts = {},
+    dependencies = {
+        "JoosepAlviste/nvim-ts-context-commentstring",
+    },
     config = function()
+        require("Comment").setup({
+            pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+        })
         vim.keymap.set("n", "<leader>/", function()
             require("Comment.api").toggle.linewise.current()
         end, { desc = "Toggle commen" })
