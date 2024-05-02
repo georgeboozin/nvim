dd628e50dc20db17f080dd71e2ff600ae27e697a
George Boozin <boozingeorge@gmail.com>
2024-05-02 16:16:52 +0300

    fix prettierd

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -17,8 +17,10 @@ return {
             -- Disable "format_on_save lsp_fallback" for languages that don't
             -- have a well standardized coding style. You can add additional
             -- languages here or re-enable it for the disabled ones.
-            local disable_filetypes =
-                { c = true, cpp = true, js = true, jsx = true, ts = true, tsx = true, mjs = true, cjs = true }
+            local disable_filetypes = {
+                c = true,
+                cpp = true, --[[ js = true, jsx = true, ts = true, tsx = true, mjs = true, cjs = true ]]
+            }
             return {
                 timeout_ms = 500,
                 lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
@@ -31,7 +33,10 @@ return {
             --
             -- You can use a sub-list to tell conform to run *until* a formatter
             -- is found.
-            -- javascript = { { "prettierd", "prettier" } },
+            javascript = { { "prettierd" } },
+            typescript = { { "prettierd" } },
+            javascriptreact = { { "prettierd" } },
+            typescriptreact = { { "prettierd" } },
         },
     },
 }
