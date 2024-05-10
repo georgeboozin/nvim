b9d35373cd13ec5e521675cb8c4d9fac384ea164
George Boozin <boozingeorge@gmail.com>
2024-05-10 14:38:06 +0300

    workaround folding for diffview

--- lua/custom/plugins/git/diffview.lua
+++ lua/custom/plugins/git/diffview.lua
@@ -5,5 +5,29 @@ return {
         map("n", "<leader>gdo", "<cmd> DiffviewOpen<CR>", { desc = "Open git diffview" })
         map("n", "<leader>gdc", "<cmd> DiffviewClose<CR>", { desc = "Close git diffview" })
         map("n", "<leader>gdh", "<cmd> DiffviewFileHistory<CR>", { desc = "Open File History" })
+        require("diffview").setup({
+            hooks = {
+                diff_buf_read = function(bufnr)
+                    vim.opt_local.foldmethod = "expr"
+                    vim.opt_local.foldexpr = "nvim_treesitter#foldexpr()"
+                    vim.opt_local.foldcolumn = "0" -- '0' or '1' is not bad
+                    vim.opt_local.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the valueufo
+                    vim.opt_local.foldlevelstart = 99
+                    vim.opt_local.foldenable = true
+                    vim.opt.foldmethod = "expr"
+                    vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+                    -- print("diff_buf_read!")
+                end,
+                view_opened = function(view)
+                    vim.opt_local.foldmethod = "expr"
+                    vim.opt_local.foldexpr = "nvim_treesitter#foldexpr()"
+                    vim.opt.foldmethod = "expr"
+                    vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+                    --
+                    -- print(("A new %s was opened on tab page %d!"):format(view.class:name(), view.tabpage))
+                end,
+            },
+        })
     end,
 }
